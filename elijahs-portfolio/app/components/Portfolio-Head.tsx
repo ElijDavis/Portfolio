814cc4dc211c72497d1f107387263c1712c5bb49
ElijDavis <elijahadavis@hotmail.com>
2025-06-02 16:23:53 -0400

    added the head componenent, deleted unecessary files and modified the home page

---
+++ elijahs-portfolio/app/components/Portfolio-Head.tsx
@@ -0,0 +1,9 @@
+import react from "react";
+
+export function Header (){
+    return (
+        <div className="bg-gradient-to-br from-emerald-500 to-sky-500">
+            
+        </div>
+    );
+}
