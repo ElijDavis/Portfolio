81a9a4eeb3bcb80f24d72e0adde2a133a76d6332
ElijDavis <elijahadavis@hotmail.com>
2025-06-02 16:28:09 -0400

    added flex row for buttons

--- elijahs-portfolio/app/components/Portfolio-Head.tsx
+++ elijahs-portfolio/app/components/Portfolio-Head.tsx
@@ -2,8 +2,10 @@ import react from "react";
 
 export function Header (){
     return (
-        <div className="bg-gradient-to-br from-emerald-500 to-sky-500">
-            
+        <div className="bg-gradient-to-br from-emerald-500 to-sky-500 flex-row ">
+            <button className="bg-black">Contact</button>
+            <button className="bg-black">experience</button>
+            <h1>Portfolio</h1>
         </div>
     );
 }
