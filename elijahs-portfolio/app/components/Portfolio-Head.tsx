74f7512032934395068246139ce31258e4d4f4e7
ElijDavis <elijahadavis@hotmail.com>
2025-06-02 22:30:30 -0400

    Added some content

--- elijahs-portfolio/app/components/Portfolio-Head.tsx
+++ elijahs-portfolio/app/components/Portfolio-Head.tsx
@@ -2,10 +2,10 @@ import react from "react";
 
 export function Header (){
     return (
-        <div className="bg-gradient-to-br from-emerald-500 to-sky-500 flex-row ">
-            <button className="bg-black">Contact</button>
-            <button className="bg-black">experience</button>
-            <h1>Portfolio</h1>
+        <div className=" items-stretch bg-gradient-to-br from-emerald-500 to-sky-500 flex-row rounded-b-lg">
+            <button className="bg-black rounded-md flex-1 py-50">Contact</button>
+            <button className="bg-black rounded-md flex-1">experience</button>
+            <h1 className="color-black flex-none">Portfolio</h1>
         </div>
     );
 }
