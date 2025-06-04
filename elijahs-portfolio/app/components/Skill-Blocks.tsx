34758af882cba339f2c95dc49b5bbca1eef3719c
ElijDavis <elijahadavis@hotmail.com>
2025-06-04 09:23:56 -0400

    Created the skill blocks, now i have to style - after I have to add functionality to get to the next pages, but maybe I should create the next pages first so I actually have something to connect to

--- elijahs-portfolio/app/components/Skill-Blocks.tsx
+++ elijahs-portfolio/app/components/Skill-Blocks.tsx
@@ -1,7 +1,11 @@
-export function skill_block (){
+type Props = {
+    title?: string;
+};
+
+export function Skill_block ({title}:Props){
     return (
-        <div>
-            <div></div>
+        <div className="h-64 w-64 bg-gradient-to-br from-rose-600 to-amber-600 rounded-2xl">
+            <p>{title}</p>
         </div>
     );
 }
--- elijahs-portfolio/app/page.tsx
+++ elijahs-portfolio/app/page.tsx
@@ -1,11 +1,16 @@
 import Image from "next/image";
 import {Header, Summary} from "./components/Portfolio-Head";
+import { Skill_block } from "./components/Skill-Blocks";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <Summary />
+      <div className="grid-cols-3 justify-center mt-20">
+        <Skill_block />
+        <Skill_block />
+      </div>
     </div>
   );
 }
