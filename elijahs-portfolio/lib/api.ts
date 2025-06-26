739d80c45b1d9691575e654101ec79e98e8e107d
ElijDavis <elijahadavis@hotmail.com>
2025-06-26 14:42:29 -0400

    changed the name of the first api function

--- elijahs-portfolio/lib/api.ts
+++ elijahs-portfolio/lib/api.ts
@@ -6,7 +6,7 @@ export type Max2ndMax = {
 
 const API_BASE = process.env.NEXT_PUBLIC_API_URL;
 
-export async function callJavaApi(input: string): Promise<Max2ndMax> {
+export async function Max2Max(input: string): Promise<Max2ndMax> {
   const res = await fetch(`${API_BASE}/api/problem1/run-json?input=${encodeURIComponent(input)}`);
   if (!res.ok) throw new Error("API call failed");
   return await res.json();
