4e9a41fa654510e0d1f135f443d308a6b745bf39
ElijDavis <elijahadavis@hotmail.com>
2025-07-07 16:18:12 -0400

    ESLint rule complaint fixes

--- elijahs-portfolio/app/page.tsx
+++ elijahs-portfolio/app/page.tsx
@@ -1,24 +1,17 @@
 'use client';
 
 import {Header} from "../components/Header";
-import { Skill_block } from "../components/Skill-Blocks";
-import { Tool } from "../components/Tool_Bar";
-import Link from "next/link";
+//import { Skill_block } from "../components/Skill-Blocks";
+//import { Tool } from "../components/Tool_Bar";
+//import Link from "next/link";
 import NavBar from "../components/NavBar";
 
 
 export default function Home() {
   return (
-    <div className="grid conten-center">
+    <div className="flex justify-center">
       <NavBar/>
       <Header/>
-      {/*<div className="flex flex-row justify-center gap-x-20 md:gap-x-32 mt-12 md:mt-24">
-        <Skill_block title={"Photography"}/>
-        <div className="bg-transparent-80 ">
-          <Link href="/Algorithms"><Skill_block title={"Data Structures and Algorithms"}/></Link>
-        </div>
-        <Skill_block title={"Fashion Design"}/>
-      </div>*/}
     </div>
   );
 }
