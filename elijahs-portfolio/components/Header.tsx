df44b7d205c1532681fc27b4cb7fb467dadf4e2a
ElijDavis <elijahadavis@hotmail.com>
2025-07-07 17:41:08 -0400

    fixed the Projects issue. Trying to get a gradient on the underline if possible. Still can't fix the background image

--- elijahs-portfolio/components/Header.tsx
+++ elijahs-portfolio/components/Header.tsx
@@ -4,7 +4,7 @@ export function Header (){
     return (
         <div className="md:mt-36 mt-10 font-[Oranienbaum] text-center text-white">
             <h1 className="text-8xl">Elijah Davis</h1>
-            <h2 className="text-4xl mt-10 underline">Full-Stack Web & API Developer</h2>
+            <h2 className="text-4xl mt-10 underline underline-offset-2 background-blue">Full-Stack Web & API Developer</h2>
         </div>
     );
 }
