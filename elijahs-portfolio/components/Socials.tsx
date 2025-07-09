46624ef0b7f71a79887feee205bb648ab86b1baf
ElijDavis <elijahadavis@hotmail.com>
2025-07-08 23:48:33 -0400

    I added more functionality to the website

--- elijahs-portfolio/components/Socials.tsx
+++ elijahs-portfolio/components/Socials.tsx
@@ -7,20 +7,25 @@ import { useState } from "react";
 export function Socials () {
   const [drop, SetDrop] = useState(false);
   return(
-    <div className="absolute right-10 pl-5 pr-5 pb-2 pt-2 z-50 flex flex-row justify-between select-none rounded-2xl cursor-pointer bg-gradient-to-r from-purphaze to-DS2MidPink p-4" onClick={() => SetDrop(!drop)}>
+    <div className="absolute right-10 z-50 flex flex-row justify-between select-none rounded-2xl cursor-pointer border-3 bg-gradient-to-r from-purphaze to-DS2MidPink transition-all duration-200 ease-in-out" onClick={() => SetDrop(!drop)}>
       {drop ? (
-        <div className="grid grid-cols-6 gap-x-5 content-center" >
-          <IconCircle icon="/icons/GitHub.png" link="https://github.com/ElijDavis" />
+        <div className="grid grid-cols-7 gap-x-5 justify-between content-center bg-black p-2" >
+          <IconCircle icon="/icons/Github1.png" link="https://github.com/ElijDavis" />
           <IconCircle icon="/icons/LinkedIn.png" link="https://www.linkedin.com/in/elijah-davis-7823ny/" />
           <IconCircle icon="/icons/youtube.png" link="https://www.youtube.com/channel/UCa4X17rOT6q3kohFtT33jzg" />
           <IconCircle icon="/icons/twitch.png" link="https://www.twitch.tv/elijahdavisdev" />
           <IconCircle icon="/icons/tiktok.png" link="https://www.tiktok.com/@elijahdavisdev" />
           <IconCircle icon="/icons/Outlook.png" link="elijahadavis1@outlook.com" />
+          <div className="order-first bg-gradient-to-r from-purphaze to-DS2MidPink rounded-2xl content-center">
+            <Image src="/icons/arrow.png" alt="arrow" width={40} height={40} className="pl-3 content-center" />
+          </div>
         </div>
       ) : (
-        <h1 className="text-center rounded-full content-center text-2xl font-[Oranienbaum] tracking-widest cursor-pointer" >
-          Socials
-        </h1>
+        <div className="pl-5 pr-5 pb-2 pt-2 z-50 flex flex-row justify-between gap-x-5 select-none rounded-2xl cursor-pointer bg-gradient-to-r from-purphaze to-DS2MidPink p-4">
+          <h1 className="text-center rounded-full content-center text-2xl font-[Oranienbaum] tracking-widest cursor-pointer" >
+            Socials
+          </h1>
+        </div>
       )}
     </div>
   );
@@ -31,10 +36,10 @@ type IconCircleProps = {
   link: string;
 };
 
-const IconCircle = ({ icon, link }: IconCircleProps) => {
-  return (
-    <div className="w-12 h-12 rounded-full bg-white">
-      <Link href={link}><Image src={icon} alt="media" width={40} height={40} className="" /></Link>
-    </div>
-  );
-};
+const IconCircle = ({ icon, link}: IconCircleProps) => (
+  <div className="w-12 h-12 rounded-full ">
+    <Link href={link}>
+      <Image src={icon} alt="media" width={40} height={40} className="" />
+    </Link>
+  </div>
+);
