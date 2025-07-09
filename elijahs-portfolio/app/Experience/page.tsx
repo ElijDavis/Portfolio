96cdcd0b4222962bac38ec8fb3917ddfcd07aafd
ElijDavis <elijahadavis@hotmail.com>
2025-07-09 19:05:22 -0400

    Fixed the look of the socails tool bar. Fix the email and tiktok extension

--- elijahs-portfolio/app/Experience/page.tsx
+++ elijahs-portfolio/app/Experience/page.tsx
@@ -3,7 +3,7 @@ const Experience = () => {
         <div className="relative min-h-screen bg-black bg-cover bg-center bg-no-repeat">
             <div className="absolute inset-0 flex items-center justify-center z-20 text-4xl bg-gradient-to-br from-moodyblue to-darkcoral">
                 <h1 className="">Experience: </h1>
-                <h2> Under Contruction</h2>
+                <h2 className=""> Under Contruction</h2>
             </div>
         </div>
     )
