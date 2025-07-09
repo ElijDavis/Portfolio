5cd81331a2a5c41953d3a679b6bfd4236f736e61
ElijDavis <elijahadavis@hotmail.com>
2025-07-09 12:17:20 -0400

    Made the main UI, I'll start applying today

--- elijahs-portfolio/app/Experience/page.tsx
+++ elijahs-portfolio/app/Experience/page.tsx
@@ -2,7 +2,8 @@ const Experience = () => {
     return(
         <div className="relative min-h-screen bg-black bg-cover bg-center bg-no-repeat">
             <div className="absolute inset-0 flex items-center justify-center z-20 text-4xl bg-gradient-to-br from-moodyblue to-darkcoral">
-                <h1 className="">Experience</h1>
+                <h1 className="">Experience: </h1>
+                <h2> Under Contruction</h2>
             </div>
         </div>
     )
--- elijahs-portfolio/app/Projects/page.tsx
+++ elijahs-portfolio/app/Projects/page.tsx
@@ -2,7 +2,8 @@ const Projects = () => {
     return(
         <div className="relative min-h-screen bg-black bg-cover bg-center bg-no-repeat">
             <div className="absolute inset-0 flex items-center justify-center z-20 text-4xl bg-gradient-to-br from-moodyblue to-darkcoral">
-                <h1 className="">Projects</h1>
+                <h1 className="">Projects: </h1>
+                <h2> Under Contruction</h2>
             </div>
         </div>
     )
