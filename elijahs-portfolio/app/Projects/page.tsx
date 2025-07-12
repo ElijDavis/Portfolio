df1f8f709ff242cbf6fe04971fc851ff2710cfb5
ElijDavis <elijahadavis@hotmail.com>
2025-07-11 20:06:17 -0400

    finnished all api imports. Implement into algorithms page next

--- elijahs-portfolio/app/Projects/page.tsx
+++ elijahs-portfolio/app/Projects/page.tsx
@@ -1,9 +1,12 @@
+import Link from "next/link";
+
 const Projects = () => {
     return(
         <div className="relative min-h-screen bg-black bg-cover bg-center bg-no-repeat">
             <div className="absolute inset-0 flex items-center justify-center z-20 text-4xl bg-gradient-to-br from-moodyblue to-darkcoral">
                 <h1 className="">Projects: </h1>
                 <h2> Under Contruction</h2>
+                <Link href="/Algorithms"><p> Algorithms</p></Link>
             </div>
         </div>
     )
--- elijahs-portfolio/lib/api.ts
+++ elijahs-portfolio/lib/api.ts
@@ -1,16 +1,111 @@
 // lib/api.ts
-export type Max2ndMax = {
-  max: number;
-  max2nd: number;
-};
+import { Max2ndMax, CGragh, Graph, DFSGraph} from "./def";
 
 const API_BASE = process.env.NEXT_PUBLIC_API_URL;
 
-export async function Max2Max(input: string): Promise<Max2ndMax> {
-  const res = await fetch(`${API_BASE}/api/problem1/run-json?input=${encodeURIComponent(input)}`);
+//2
+export async function found(input: string): Promise<boolean> {
+  const res = await fetch(`${API_BASE}/assignment2/problem1?input=${encodeURIComponent(input)}`);
   if (!res.ok) throw new Error("API call failed");
   return await res.json();
 }
+
+export async function count_(input: string): Promise<number> {
+  const res = await fetch(`${API_BASE}/assignment2/problem2?input=${encodeURIComponent(input)}`);
+  if (!res.ok) throw new Error("API call failed");
+  return await res.json();
+}
+
+export async function sqrt(input: string): Promise<number> {
+  const res = await fetch(`${API_BASE}/assignment2/problem3?input=${encodeURIComponent(input)}`);
+  if (!res.ok) throw new Error("API call failed");
+  return await res.json();
+}
+
+
+//3
+export async function biggie(input: string): Promise<number> {
+  const res = await fetch(`${API_BASE}/assignment3/problem1?input=${encodeURIComponent(input)}`);
+  if (!res.ok) throw new Error("API call failed");
+  return await res.json();
+}
+
+export async function Count_(input: string): Promise<number> {
+  const res = await fetch(`${API_BASE}/assignment3/problem2?input=${encodeURIComponent(input)}`);
+  if (!res.ok) throw new Error("API call failed");
+  return await res.json();
+}
+
+
+//4
+export async function Max2Max(input: string): Promise<Max2ndMax> {
+  const res = await fetch(`${API_BASE}/api/assignment4/problem1?input=${encodeURIComponent(input)}`);
+  if (!res.ok) throw new Error("API call failed");
+  return await res.json();
+}
+
+
+//5
+export async function union(input: string): Promise<number[]> {
+  const res = await fetch(`${API_BASE}/assignment5/problem1/union?input=${encodeURIComponent(input)}`);
+  if (!res.ok) throw new Error("API call failed");
+  return await res.json();
+}
+
+export async function intersection(input: string): Promise<number[]> {
+  const res = await fetch(`${API_BASE}/assignment5/problem1/intersection?input=${encodeURIComponent(input)}`);
+  if (!res.ok) throw new Error("API call failed");
+  return await res.json();
+}
+
+export async function threeWayP(input: string): Promise<number[]> {
+  const res = await fetch(`${API_BASE}/assignment5/problem2?input=${encodeURIComponent(input)}`);
+  if (!res.ok) throw new Error("API call failed");
+  return await res.json();
+}
+
+
+//6
+export async function ccreateGraph(input: CGragh): Promise<CGragh> {
+  const res = await fetch(`${API_BASE}/api/assignment6/problem1/createGraph`, {
+    method: "POST",
+    body: JSON.stringify(input),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!res.ok) throw new Error("API call failed");
+  return await res.json();
+}
+
+export async function cdfs(input: DFSGraph): Promise<String> {
+  const res = await fetch(`${API_BASE}/api/assignment6/problem1/DFS`, {
+    method: "POST",
+    body: JSON.stringify(input),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!res.ok) throw new Error("API call failed");
+  return await res.json();
+}
+
+export async function AdjList(input: Graph): Promise<String> {
+  const res = await fetch(`${API_BASE}/api/assignment6/problem1/getAdjacencyList`, {
+    method: "POST",
+    body: JSON.stringify(input),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!res.ok) throw new Error("API call failed");
+  return await res.json();
+}
+
+//all API calls should begin with the API_BASE variable and /api
 
 //When using the count_ function to find the number of occurrences of a double in a list
 //the input should be a string of comma-separated numbers, e.g. "1,2,3,4,5", and the number to count will be added at the end of the string.
